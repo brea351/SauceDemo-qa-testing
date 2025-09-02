117473da87eb5286e50770e2b8d9a8d99f87d514
brea351 <richydainty.ot@gmail.com>
2025-09-02 10:53:04 +0100

    chore: add junit reporter for Jenkins integration

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.cy.js",
-    baseUrl: "https://www.saucedemo.com/",
-    chromeWebSecurity: false
-  }
+    setupNodeEvents(on, config) {
+      // You can add custom tasks here if needed
+    },
+    baseUrl: "https://www.saucedemo.com", // your app’s base URL
+    reporter: "junit",
+    reporterOptions: {
+      mochaFile: "results/test-results-[hash].xml",
+      toConsole: true
+    },
+  },
 });
+
